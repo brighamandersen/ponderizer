e3b3bcb3aff7ac2b8792987ce305a850f0705e1c
Brigham Andersen <brighamband@gmail.com>
2021-03-16 17:50:31 -0600

    Added not found route

---
+++ src/mock-data.js
@@ -0,0 +1,18 @@
+const mock = [
+  {
+    id: 1,
+    book: 'Philippians',
+    chapter: 4,
+    verseNum: 13,
+    content: 'I can do all things through Christ which strengtheneth me.',
+  },
+  {
+    id: 2,
+    book: 'John',
+    chapter: 15,
+    verseNum: 13,
+    content: 'Greater love hath no man than this, that a man lay down his life for his friends.',
+  },
+];
+
+export default mock;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="not-found">
+      <h1>404 - Not Found Error</h1>
+      <p>Sorry, the page you are looking for could not be found</p>
+      <router-link to="/" tag="button">GO HOME</router-link>
+  </div>
+</template>
+
+<style scoped>
+  .not-found {
+    text-align: left;
+    padding: 5rem;
+  }
+</style>
