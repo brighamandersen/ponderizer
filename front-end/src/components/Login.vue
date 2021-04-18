94bf42d9a779048c2e6a86dc498e4cf2887d083f
Brigham Andersen <brighamband@gmail.com>
2021-04-18 00:20:44 -0600

    Mostly fixed styling for login (just shrink down to fit on one page

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -1,29 +1,45 @@
 <template>
 <div class="hero">
   <div class="heroBox">
-    <form class="pure-form">
+    <form class="card default-form">
       <fieldset>
         <legend>Register for an account</legend>
-        <input placeholder="first name" v-model="firstName">
-        <input placeholder="last name" v-model="lastName">
-      </fieldset>
-      <fieldset>
-        <input placeholder="username" v-model="username">
-        <input type="password" placeholder="password" v-model="password">
-      </fieldset>
-      <fieldset>
-        <button type="submit" class="pure-button pure-button-primary" @click.prevent="register">Register</button>
+        <div class="flex-parent form-section">
+          <label class="flex-child">First Name</label>
+          <input type="text" class="flex-child" placeholder="First Name" autofocus required v-model="firstName">
+        </div>
+        <div class="flex-parent form-section">
+          <label class="flex-child">Last Name</label>
+          <input type="text" class="flex-child" placeholder="Last Name" autofocus required v-model="lastName">
+        </div>
+        <div class="flex-parent form-section">
+          <label class="flex-child">Username</label>
+          <input type="text" class="flex-child" placeholder="Username" autofocus required v-model="username">
+        </div>
+        <div class="flex-parent form-section">
+          <label class="flex-child">Password</label>
+          <input type="text" class="flex-child" placeholder="Password" autofocus required v-model="password">
+        </div>
+        <div class="flex-parent btn-wrapper">
+          <button class="flex-child" type="submit" @click.prevent="register">Register</button>
+        </div>
       </fieldset>
     </form>
     <p v-if="error" class="error">{{error}}</p>
-    <form class="pure-form space-above">
+    <form class="card default-form space-above">
       <fieldset>
         <legend>Login</legend>
-        <input placeholder="username" v-model="usernameLogin">
-        <input type="password" placeholder="password" v-model="passwordLogin">
-      </fieldset>
-      <fieldset>
-        <button type="submit" class="pure-button pure-button-primary" @click.prevent="login">Login</button>
+        <div class="flex-parent form-section">
+          <label class="flex-child">Username</label>
+          <input type="text" class="flex-child" placeholder="Username" autofocus required v-model="usernameLogin">
+        </div>
+        <div class="flex-parent form-section">
+          <label class="flex-child">Password</label>
+          <input type="text" class="flex-child" placeholder="Password" autofocus required v-model="passwordLogin">
+        </div>
+        <div class="flex-parent btn-wrapper">
+          <button class="flex-child" type="submit" @click.prevent="login">Login</button>
+        </div>
       </fieldset>
     </form>
     <p v-if="errorLogin" class="error">{{errorLogin}}</p>
@@ -113,10 +129,6 @@ h1 {
 
 .hero form legend {
   font-size: 20px;
-}
-
-input {
-  margin-right: 10px;
 }
 
 .error {
