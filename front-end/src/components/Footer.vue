15242528f182d0882fa2804571ad3497de07245a
Brigham Andersen <brighamband@gmail.com>
2023-07-31 22:46:48 -0600

    Update footer GH link

--- front-end/src/components/Footer.vue
+++ front-end/src/components/Footer.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="navbar-footer" id="footer">
     <p class="flex-child">Ponderizer</p>
-    <a class="flex-child" 
-      href="https://github.com/brighamband/ponderizer"
-    >
-    See Source Code
+    <a class="flex-child" href="https://github.com/brighamandersen/ponderizer">
+      See Source Code
     </a>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Footer',
-  };
+export default {
+  name: "Footer"
+};
 </script>
 
 <style scoped>
-  #footer {
-    display: flex;
-    align-items: center;
-    text-align: center;
-  }
-  
-  a {
-    color: rgb(187, 187, 187);
-  }
+#footer {
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
 
-  i {
-    font-size: small;
-  }
+a {
+  color: rgb(187, 187, 187);
+}
+
+i {
+  font-size: small;
+}
 </style>
